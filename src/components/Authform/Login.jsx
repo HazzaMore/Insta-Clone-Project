dcfd76896a777a686d38dc5d26287fe355e7e567
HazzaMore <mrhazar.moore@gmail.com>
2024-11-01 16:07:42 +0000

    Added login functionality

--- src/components/Authform/Login.jsx
+++ src/components/Authform/Login.jsx
@@ -1,5 +1,8 @@
-import { Input, Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
+import { Button } from "../ui/button";
+import { Alert } from "../ui/alert";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -7,6 +10,7 @@ const Login = () => {
     password: "",
   });
 
+  const { loading, error, login } = useLogin();
   return (
     <>
       <Input
@@ -29,8 +33,20 @@ const Login = () => {
         size={"sm"}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          {error.message}
+        </Alert>
+      )}
 
-      <Button w={"full"} colorPalette="blue" size={"sm"} fontSize={14}>
+      <Button
+        w={"full"}
+        colorPalette="blue"
+        size={"sm"}
+        fontSize={14}
+        loading={loading}
+        onClick={() => login(inputs)}
+      >
         Login
       </Button>
     </>
